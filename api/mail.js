34499c2c92d0747876ccfaedb291f1d6f17f5a20
ButlerFuqua <butlerfuqua@gmail.com>
2021-06-21 10:58:15 -1000

    Change from to verified sender

--- api/mail.js
+++ api/mail.js
@@ -10,12 +10,9 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 module.exports = async (req, res) => {
 
-    console.log("process.env.SENDGRID_EMAIL", process.env.SENDGRID_EMAIL)
-    console.log("process.env.SENDGRID_API_KEY", process.env.SENDGRID_API_KEY)
-
     const msg = {
         to: process.env.SENDGRID_EMAIL, // Change to your recipient
-        from: 'test@example.com', // Change to your verified sender
+        from: process.env.SENDGRID_EMAIL, // Change to your verified sender
         subject: 'Sending with SendGrid is Fun',
         text: 'and easy to do anywhere, even with Node.js',
         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
@@ -32,7 +29,7 @@ module.exports = async (req, res) => {
         })
         .catch((error) => {
             console.error(error)
-            return res.status(500).json({
+            return res.status(error.code).json({
                 success: false,
                 message: `Email NOT sent:\n\n\r\r${error.message}`,
                 error
