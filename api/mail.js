eec544aeb4d75e6a716889ef4c0025de75daf08c
ButlerFuqua <butlerfuqua@gmail.com>
2021-06-21 11:04:01 -1000

    comment out send code

--- api/mail.js
+++ api/mail.js
@@ -10,30 +10,36 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 module.exports = async (req, res) => {
 
-    const msg = {
-        to: process.env.SENDGRID_EMAIL, // Change to your recipient
-        from: process.env.SENDGRID_EMAIL, // Change to your verified sender
-        subject: 'Sending with SendGrid is Fun',
-        text: 'and easy to do anywhere, even with Node.js',
-        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    }
 
-    sgMail
-        .send(msg)
-        .then(() => {
-            console.log('Email sent')
-            return res.status(200).json({
-                success: true,
-                message: 'Email sent'
-            })
-        })
-        .catch((error) => {
-            console.error(error)
-            return res.status(error.code).json({
-                success: false,
-                message: `Email NOT sent:\n\n\r\r${error.message}`,
-                error
-            })
-        })
+    return res.status(200).json({
+        success: true,
+        message: `Works, but waiting`,
+    })
+
+    // const msg = {
+    //     to: process.env.SENDGRID_EMAIL, // Change to your recipient
+    //     from: process.env.SENDGRID_EMAIL, // Change to your verified sender
+    //     subject: 'Sending with SendGrid is Fun',
+    //     text: 'and easy to do anywhere, even with Node.js',
+    //     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    // }
+
+    // sgMail
+    //     .send(msg)
+    //     .then(() => {
+    //         console.log('Email sent')
+    //         return res.status(200).json({
+    //             success: true,
+    //             message: 'Email sent'
+    //         })
+    //     })
+    //     .catch((error) => {
+    //         console.error(error)
+    //         return res.status(error.code).json({
+    //             success: false,
+    //             message: `Email NOT sent:\n\n\r\r${error.message}`,
+    //             error
+    //         })
+    //     })
 
 }
